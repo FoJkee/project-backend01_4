923e4804c88929940194d75db85fc2726fd972af
FoJkee <romanovsky0815@gmail.com>
2023-06-20 17:23:56 +0300

    fix6

--- src/middleware/authorize.ts
+++ src/middleware/authorize.ts
@@ -3,7 +3,7 @@ import {Request, Response,NextFunction} from "express";
 
 export const authorizeMiddleware = async (req: Request, res: Response, next: NextFunction) => {
 
-    const code = await Buffer.from('superadmin:qwerty', 'utf-8').toString('base64')
+    const code = await Buffer.from('admin:qwerty', 'utf-8').toString('base64')
 
     if(req.headers.authorization === `Basic ${code}`){
         next()
