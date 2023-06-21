5b693e51a39699e984cee84cf6049805f0919747
FoJkee <romanovsky0815@gmail.com>
2023-06-21 16:24:32 +0300

    fix18

--- src/middleware/blogs-middleware.ts
+++ src/middleware/blogs-middleware.ts
@@ -5,13 +5,13 @@ const pattern = "^https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/
 
 
 export const blogsMiddleware = [
-    body('name').exists().withMessage('Incorrect name').isString()
+    body('name').exists().trim().withMessage('Incorrect name').isString()
         .isLength({min: 1, max: 15}).withMessage('String length is not more than 15 symbols'),
-    body('description').exists().isString().isLength({
+    body('description').exists().trim().isString().isLength({
         min: 1,
         max: 500
     }).withMessage('String length is not more than 500 symbols'),
-    body('websiteUrl').exists().isString().isLength({
+    body('websiteUrl').exists().trim().isString().isLength({
         min: 1,
         max: 100
     }).withMessage("String length is not more than 100 symbols").matches(pattern).withMessage('Incorrect websiteUrl')
